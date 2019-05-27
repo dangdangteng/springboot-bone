3ce6ae47553e4e04b405f510a07d8495f8b88d92
huxueyan <huxueyan101@gmail.com>
2019-05-27 22:56:29 +0800

    springboot 组件学习-深入理解

---
+++ aredis/src/main/java/xiaobai/angle/aredis/AredisApplication.java
@@ -0,0 +1,13 @@
+package xiaobai.angle.aredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AredisApplication.class, args);
+	}
+
+}
---
+++ aredis/src/test/java/xiaobai/angle/aredis/AredisApplicationTests.java
@@ -0,0 +1,16 @@
+package xiaobai.angle.aredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AredisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
