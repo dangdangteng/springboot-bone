0add0184a017f606c0f351808dcef697f2d64ee3
huxueyan <huxueyan101@gmail.com>
2019-08-07 13:53:24 +0800

    1

---
+++ eurake/src/main/java/com/dinglicom/cloud/eurake/config/CrosConfig.java
@@ -0,0 +1,11 @@
+package com.dinglicom.cloud.eurake.config;
+
+/***
+ * @creation purpose : 跨域
+ * @data : 10:33,2019-07-24
+ * @author : 『Ezreal』
+ * @Character description : 最帅的那个就是我
+ ***/
+public class CrosConfig {
+
+}
---
+++ eurake/src/main/java/com/dinglicom/cloud/eurake/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.dinglicom.cloud.eurake.config;
+
+/***
+ * @creation purpose : security 权限验证
+ * @data : 16:44,2019-07-23
+ * @author : 『Ezreal』
+ * @Character description : 最帅的那个就是我
+ ***/
+public class SecurityConfig {
+
+}
