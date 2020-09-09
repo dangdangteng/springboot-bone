d57649e0521012847705ef9d14d4bf3ef798692a
huxueyan <huxueyan101@gmail.com>
2020-09-09 14:28:50 +0800

    feat:

---
+++ a_test/src/main/java/sout/SystemOut.java
@@ -0,0 +1,12 @@
+package sout;
+
+/**
+ * @author baiyang
+ * @date 2020/9/9
+ */
+public class SystemOut {
+
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+    }
+}
