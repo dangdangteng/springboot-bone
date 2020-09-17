b7484874c77d5d49ec24faa5458c01634be6132d
huxueyan <huxueyan101@gmail.com>
2020-09-17 11:54:14 +0800

    feat:

---
+++ java/src/main/java/com/loubi/java/AlibabaruleApplication.java
@@ -0,0 +1,13 @@
+package com.loubi.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaruleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibabaruleApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/loubi/java/AlibabaruleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loubi.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaruleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
